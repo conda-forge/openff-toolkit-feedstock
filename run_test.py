6994d20b611f1f0c16feb4bf71378478a42cd33b
Matthew W. Thompson <mattwthompson@protonmail.com>
2021-01-26 08:52:31 -0600

    Add print in test script

--- run_test.py
+++ run_test.py
@@ -1,4 +1,11 @@
-from openff.toolkit.utils.toolkits import AmberToolsToolkitWrapper, RDKitToolkitWrapper
+from openff.toolkit.utils.toolkits import (
+    GLOBAL_TOOLKIT_REGISTRY,
+    AmberToolsToolkitWrapper,
+    RDKitToolkitWrapper,
+)
 
 assert RDKitToolkitWrapper().is_available()
 assert AmberToolsToolkitWrapper().is_available()
+
+
+print(GLOBAL_TOOLKIT_REGISTRY.registered_toolkit_versions)
