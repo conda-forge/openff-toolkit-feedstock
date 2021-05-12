35da315fdc9b78d583307c8f4ac7d0b488f5f951
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-05-12 15:55:38 +1000

    Bugfixes for install script

--- recipe/move-examples.sh
+++ recipe/move-examples.sh
@@ -1,5 +1,11 @@
-mkdir -p $CONDA_PREFIX/share/openff-toolkit
-cp -r examples $CONDA_PREFIX/share/openff-toolkit
+#! /bin/bash
 
-mv $CONDA_PREFIX/share/openff-toolkit/examples/examples_helper.py $CONDA_PREFIX/bin/openff-toolkit-examples
-chmod +x $CONDA_PREFIX/bin/openff-toolkit-examples
+# Shell safety: Exit script on error, treat unset variables as error, propagate errors out of pipelines
+set -eu -o pipefail
+
+mkdir -p "$CONDA_PREFIX/share/openff-toolkit"
+cp -r examples "$CONDA_PREFIX/share/openff-toolkit"
+
+mkdir -p "$CONDA_PREFIX/bin"
+mv "$CONDA_PREFIX/share/openff-toolkit/examples/examples_helper.py" "$CONDA_PREFIX/bin/openff-toolkit-examples"
+chmod +x "$CONDA_PREFIX/bin/openff-toolkit-examples"
