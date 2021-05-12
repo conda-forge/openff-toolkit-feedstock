dd74898958531bc04ccf515357e1d73b3cc5b033
Josh Mitchell <yoshanuikabundi@gmail.com>
2021-05-12 15:39:19 +1000

    Update openff-toolkit-examples scripts for new helper

--- recipe/move-examples.sh
+++ recipe/move-examples.sh
@@ -1,2 +1,5 @@
 mkdir -p $CONDA_PREFIX/share/openff-toolkit
 cp -r examples $CONDA_PREFIX/share/openff-toolkit
+
+mv $CONDA_PREFIX/share/openff-toolkit/examples/examples_helper.py $CONDA_PREFIX/bin/openff-toolkit-examples
+chmod +x $CONDA_PREFIX/bin/openff-toolkit-examples
--- recipe/test_examples_copied.py
+++ recipe/test_examples_copied.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from os import environ
+from subprocess import run
 
 
 def file_tree(path):
@@ -12,19 +13,32 @@ def file_tree(path):
 
 def main():
     """Entrypoint for this test"""
-    repo_path = Path("examples")
-    installed_path = Path(environ["CONDA_PREFIX"]) / "share/openff-toolkit/examples"
+    # Get lists of the examples files in source and installation
+    repo_tree = file_tree("examples")
+    installed_tree = file_tree(
+        Path(environ["CONDA_PREFIX"]) / "share/openff-toolkit/examples"
+    )
 
-    repo_tree = file_tree(repo_path)
-    installed_tree = file_tree(installed_path)
-
+    # Print the trees out for debugging
     print("Source repository examples tree:", *repo_tree, sep="\n    ")
     print("Installed examples tree:", *installed_tree, sep="\n    ")
 
+    # Remove the helper script, which is moved to the bin directory during installation
+    repo_tree.remove("examples_helper.py")
+
+    # Check that the (filtered) file trees match, and are not empty
     assert repo_tree, "Source repository tree is empty"
-    assert (
-        repo_tree == installed_tree
-    ), "Source repository tree doesn't match installed tree"
+    assert repo_tree == installed_tree, "Source repo tree doesn't match installed tree"
+
+    # Run the examples helper and check what files it copies over
+    helper_test_path = Path("helper_test_target")
+    helper_test_path.mkdir()
+    run("openff-toolkit-examples", cwd=helper_test_path, check=True)
+    helper_test_tree = file_tree(helper_test_path / "examples")
+
+    # Print and check the helper test
+    print("Examples helper target tree:", *helper_test_tree, sep="\n    ")
+    assert helper_test_tree == repo_tree, "Examples helper failed to install all files"
 
 
 if __name__ == "__main__":
