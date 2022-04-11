27d17a160d1e4c72cf710b218882356d8c508d9d
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-04-11 16:45:43 -0500

    Add minimal RDKit roundtrip

--- recipe/test_openff_toolkit.py
+++ recipe/test_openff_toolkit.py
@@ -13,6 +13,7 @@ print(GLOBAL_TOOLKIT_REGISTRY.registered_toolkit_versions)
 from openff.toolkit.topology import Molecule, Topology
 from openff.toolkit.typing.engines.smirnoff import ForceField
 offmol = Molecule.from_smiles('CCO')
+Molecule.from_rdkit(offmol.to_rdkit())
 ff = ForceField('openff-1.0.0.offxml')
 ff.create_openmm_system(offmol.to_topology())
 
