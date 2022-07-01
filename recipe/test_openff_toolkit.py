80e02bccce32f1c54d6c7295f9b04142d6e20f01
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-07-01 15:02:08 -0500

    Do not test create_openmm_system without Interchange installed

--- recipe/test_openff_toolkit.py
+++ recipe/test_openff_toolkit.py
@@ -14,5 +14,5 @@ from openff.toolkit import Molecule, Topology, ForceField
 offmol = Molecule.from_smiles('CCO')
 Molecule.from_rdkit(offmol.to_rdkit())
 ff = ForceField('openff-1.0.0.offxml')
-ff.create_openmm_system(offmol.to_topology())
-
+# Un-comment once Interchange builds are accessible at test time
+# ff.create_openmm_system(offmol.to_topology())
