6f38441543648f552a169504652f52f17778848c
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-05-21 15:39:22 -0500

    Try `CONDA_PREFIX_2`

--- recipe/move-examples.sh
+++ recipe/move-examples.sh
@@ -3,14 +3,14 @@
 # Shell safety: Exit script on error, treat unset variables as error, propagate errors out of pipelines
 set -eu -o pipefail
 
-echo "CONDA_PREFIX is: "
-echo $CONDA_PREFIX
+echo "CONDA_PREFIX_2 is: "
+echo $CONDA_PREFIX_2
 
 env
 
-mkdir -p "$CONDA_PREFIX/share/openff-toolkit"
-cp -r examples "$CONDA_PREFIX/share/openff-toolkit"
+mkdir -p "$CONDA_PREFIX_2/share/openff-toolkit"
+cp -r examples "$CONDA_PREFIX_2/share/openff-toolkit"
 
-mkdir -p "$CONDA_PREFIX/bin"
-mv "$CONDA_PREFIX/share/openff-toolkit/examples/examples_helper.py" "$CONDA_PREFIX/bin/openff-toolkit-examples"
-chmod +x "$CONDA_PREFIX/bin/openff-toolkit-examples"
+mkdir -p "$CONDA_PREFIX_2/bin"
+mv "$CONDA_PREFIX_2/share/openff-toolkit/examples/examples_helper.py" "$CONDA_PREFIX_2/bin/openff-toolkit-examples"
+chmod +x "$CONDA_PREFIX_2/bin/openff-toolkit-examples"
